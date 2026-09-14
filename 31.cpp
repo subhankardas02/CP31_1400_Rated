0296a8ea5b64ee140740e4af80509210746c6143
subhankar das <dassubhankardassubhankar@gmail.com>
2026-09-14 21:44:56 +0530

    30

--- 31.cpp
+++ 31.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long int
 const ll MOD=1e9+7;
-
+ 
 ll func(int val, int lim, int idx, vector<vector<ll>>& dp){
-
+ 
 	if(idx==0){
 		return 1;
 	}
@@ -15,20 +15,20 @@ ll func(int val, int lim, int idx, vector<vector<ll>>& dp){
 		if(i%val==0) ans=(ans+func(i, lim, idx-1, dp))%MOD;
 	}
 	return dp[idx][val]=ans;
-
+ 
 }
-
+ 
 int main() {
-
-
+ 
+ 
 	ll n, k;
 	cin>>n>>k;
 	
 	vector<vector<ll>> dp(k+1, vector<ll>(n+1, -1));
-
+ 
 	ll ans=func(1, n, k, dp);
-
+ 
 	cout<<ans<<endl;
 	
-
+ 
 }
