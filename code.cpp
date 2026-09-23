191e0ad9f2a8e1d9643ccba98da5f8df55de0fce
subhankar das <dassubhankardassubhankar@gmail.com>
2026-09-23 21:39:56 +0530

    codechef

--- code.cpp
+++ code.cpp
@@ -8,27 +8,44 @@ int main() {
 
 
 	ll t=1;
-	// cin>>t;
+	cin>>t;
 	while(t--){
 		ll n;
 		cin>>n;
 		vector<ll> vec;
+		vector<vector<int>> dp(n+1, vector<int>(2, -1));
 		for(int i=0; i<n; i++){
 			ll val;
 			cin>>val;
-			if(val==0) continue;
 			vec.push_back(val);
 		}
-		n=vec.size();
-		ll ans=0;
-		vector<bool> vis(n, false);
-		if(vec.size())
+		ll sum=0, minval=0;
+		bool del=false;
+		bool poss=true;
+		for(int i=0; i<n; i++){
+			if(vec[i]<minval) minval=vec[i];
+			sum=sum+vec[i];
+			if(sum<0){
+				if(del==true){
+					poss=false;
+				}
+				else{
+					sum=sum-minval;
+					del=true;
+				}
+			}
+
+		}
+
+
+		if(poss) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+		
 
 
 	}
 	
 	
-// 
 	
 
 }
